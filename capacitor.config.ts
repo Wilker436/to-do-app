3ac040854859dc9214591bead3452b7356fb922d
Wilker436 <wikerpachecoperez@gmail.com>
2025-03-15 16:42:11 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Taller-1-FireBase',
+  webDir: 'www'
+};
+
+export default config;
